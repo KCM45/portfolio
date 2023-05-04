af8eeb5c30916e7c13fe0806c1bb0b78316042c6
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-05-04 14:23:51 -0700

    Added link to Marvel project

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const marvelAPI = document.querySelector(".port-1st-label");
+marvelAPI.addEventListener("click", goToMarvel);
+
+function goToMarvel() {
+  let URL = "https://andressilva1.github.io/Group-Project/";
+  window.location.replace(URL);
+}
